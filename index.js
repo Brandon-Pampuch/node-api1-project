9ff9a2d6d666f7a738ff8fc50bb62d575349a1b1
Brandon_Pampuch <brandonpampuch@gmail.com>
2019-09-22 10:09:15 -0500

    add get request

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// implement your API here
+const express = require('express')
+const db = require('./data/db')
+
+
+const server = express()
+server.use(express.json())
+
+server.listen(3000, () => 'listening on 3000')
+
+
+
+server.get('/', (req, res) => {
+    db.find()
+        .then(data => {
+            res.json(data)
+        })
+        .catch(err => {
+            res.status(500).json({
+                err: err,
+                message: 'failed to get hubs'
+
+            })
+        })
+})
